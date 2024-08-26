58d301f3231c08e960d5d6ae702d66dc506daa50
farzad <farzadhabibii98@gmail.com>
2024-08-26 14:27:20 -0700

    update test scripts

--- ansible/roles/build/files/test.sh
+++ ansible/roles/build/files/test.sh
@@ -3,23 +3,25 @@
 # Target service to monitor
 SERVICE_NAME="client"
 # PROB_VALUES=(100)
-MODE_VALUES=("slw" "bamboo" "ww")
-DELAY_VALUES=(10 30 50)
-PROB_VALUES=(100 90 80 70 60 50 40 30 20 10 0)
-HOT_VALUES=(16 32 64 128)
-# HOT_VALUES=(64)
-NUM_OF_ITEM=(-1)
-# NUM_OF_ITEM=(-1 20 30 40)
+MODE_VALUES=("ww")
+DELAY_VALUES=(2)
+PROB_VALUES=(0)
+# PROB_VALUES=(100 90 80 70 60 50 40 30 20 10 0)
+# HOT_VALUES=(16 32 64 128)
+HOT_VALUES=(64)
+# NUM_OF_ITEM=(-1)
+NUM_OF_ITEM=(20)
 
 sudo docker-compose --compatibility down --volumes 2>/dev/null
 
 
-for MODE in "${MODE_VALUES[@]}"; do
-    for DELAY in "${DELAY_VALUES[@]}"; do
-        for ITEM in "${NUM_OF_ITEM[@]}"; do
-            for HOT in "${HOT_VALUES[@]}"; do
+for DELAY in "${DELAY_VALUES[@]}"; do
+    for ITEM in "${NUM_OF_ITEM[@]}"; do
+        for HOT in "${HOT_VALUES[@]}"; do
+            for MODE in "${MODE_VALUES[@]}"; do
                 for PROB in "${PROB_VALUES[@]}"; do
-                    echo "Starting test with PROB=$PROB and HOT_RECORDS=$HOT and ITEM_NUM=$ITEM"
+
+                    echo "Starting test for $MODE with DELAY=$DELAY PROB=$PROB and HOT_RECORDS=$HOT and ITEM_NUM=$ITEM"
 
                     # Create and populate the .env file
                     echo "PROB=$PROB" > ".env"
@@ -30,9 +32,23 @@ for MODE in "${MODE_VALUES[@]}"; do
 
                     sudo docker-compose --compatibility up -d 2>/dev/null
 
+                    # Define timeout in seconds
+                    TIMEOUT=300  # 5 minutes timeout
+                    ELAPSED=0
+                    SLEEP_INTERVAL=5
                     # Wait for the specified service to exit
                     while [ $(sudo docker ps -q -f name="$SERVICE_NAME" | wc -l) -gt 0 ]; do
-                        sleep 5  # Checks every 5 seconds
+                        if [ $ELAPSED -ge $TIMEOUT ]; then
+                            echo "Timeout reached. Service did not exit within the timeout period. restart!"
+                            sudo docker-compose --compatibility down --volumes 2>/dev/null
+                            sleep 10
+                            sudo docker-compose --compatibility up -d 2>/dev/null
+                            sleep 10
+                            ELAPSED=0
+                            echo "New Elapsed $ELAPSED"
+                        fi
+                        sleep $SLEEP_INTERVAL
+                        ELAPSED=$((ELAPSED + SLEEP_INTERVAL))
                     done
 
                     # When the service exits, bring everything down
